0bb007438e7ca97bc4828a9cd28215eb6d9224e5
yasan kahangama <yasanmkahangama@gmail.com>
2022-04-27 20:03:12 +0530

    student create,read,update,delete functions are finished

--- routes/student.routes.js
+++ routes/student.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const app = express();
+// const app = express();
 
 const studentExpressRoute = express.Router();
 let StudentSchema = require('../model/student.model');
@@ -12,7 +12,54 @@ studentExpressRoute.route('/').get((req,res,next)=>{
             console.log(data);
             res.json(data);
         }
-    })
-})
+    });
+});
+
+studentExpressRoute.route('/student/:id').get((req,res,next)=>{
+    StudentSchema.findById(req.params.id,(error,data)=>{
+     if (error) {
+         return next(error)
+     } else {
+         res.json(data)
+     }
+    });
+ });
+
+ studentExpressRoute.route('/add-student').post((req,res,next)=>{
+    StudentSchema.create(req.body,(error, data) =>{
+    if (error) {
+        return next(error)
+    } else {
+        res.json(data)
+    }
+});
+});
+
+studentExpressRoute.route('/del-student/:id').delete((req,res,next)=>{
+    StudentSchema.findByIdAndRemove(req.params.id,(error,data)=>{
+     if (error) {
+         return next(error)
+     } else {
+        res.status(200).json({
+            msg: data
+        })
+     }
+    }) 
+ })
+
+ studentExpressRoute.route('/update-student/:id').put((req,res,next)=>{
+    StudentSchema.findByIdAndUpdate(req.params.id,{
+        $set: req.body
+    },(error,data)=>{
+     if (error) {
+         return next(error)
+     } else {
+         res.json(data);
+         console.log('Deleted Successfully!')
+     }
+    }) 
+ })
+
+
 
 module.exports = studentExpressRoute;
